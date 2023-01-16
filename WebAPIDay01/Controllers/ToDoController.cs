6007f589d747e0119791ebe1874ec8d5fadf1e89
Brayden Kim <dudgjs1318@gmail.com>
2023-01-16 15:13:12 -0800

    Add project files.

---
+++ WebAPIDay01/Controllers/ToDoController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPIDay01.Models;
+using WebAPIDay01.Repository;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPIDay01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : Controller
+    {
+        private readonly TodoContext _todoContext;
+
+        public ToDoController(TodoContext todoContext)
+        {
+            _todoContext = todoContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllToDos()
+        {
+            ToDoRepository toDoRepository = new ToDoRepository(_todoContext);
+            var todos = toDoRepository.GetAllToDos();
+
+            return Ok(todos);
+        }
+
+        [HttpGet("{toDoId}")]
+        public IActionResult GetToDo(int toDoId)
+        {
+            ToDoRepository toDoRepository = new ToDoRepository(_todoContext);
+
+            var todo = toDoRepository.GetToDo(toDoId);
+
+            if(todo == null)
+            {
+                return NotFound("Id is not found");
+            }
+
+            return Ok(todo);
+        }
+
+        [HttpPost]
+        public IActionResult CreateToDo(ToDo createdToDo)
+        {
+            if (createdToDo == null)
+            {
+                return BadRequest(createdToDo);
+            }
+            ToDoRepository toDoRepository = new ToDoRepository(_todoContext);
+            toDoRepository.AddToDo(createdToDo);
+            return Ok(createdToDo);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateToDo(ToDo updatedToDo)
+        {
+          
+            ToDoRepository toDoRepository = new ToDoRepository(_todoContext);
+
+           
+            bool validateUpdatedToDo = toDoRepository.UpdateToDo(updatedToDo);
+
+            if (!validateUpdatedToDo)
+            {
+                return BadRequest("Something went wrong updating to do list");
+            }
+
+            var todo = toDoRepository.GetToDo(updatedToDo.Id);
+
+
+            return Ok(todo);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteToDo(int id)
+        {
+            ToDoRepository toDoRepository = new ToDoRepository(_todoContext);
+
+
+            bool validatedToDo = toDoRepository.DeleteToDo(id);
+
+            if (!validatedToDo)
+            {
+                return BadRequest("Something went wrong deleting to do");
+
+            }
+
+            var todos = toDoRepository.GetAllToDos();
+
+
+            return Ok(todos);
+        }
+
+    }
+}
---
+++ WebAPIDay01/Repository/ToDoRepository.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPIDay01.Models;
+
+namespace WebAPIDay01.Repository
+{
+    public class ToDoRepository
+    {
+        private readonly TodoContext _context;
+        public ToDoRepository(TodoContext context)
+        {
+            _context = context;
+        }
+
+        public ICollection<ToDo> GetAllToDos()
+        {
+            return _context.ToDos.ToList();
+        }
+
+        public ToDo GetToDo(int id)
+        {
+            return _context.ToDos.Where(td => td.Id == id).FirstOrDefault();
+        }
+
+        public void AddToDo(ToDo toDo)
+        {
+           
+           _context.Add(toDo);
+           _context.SaveChanges();
+        }
+
+        public bool UpdateToDo(ToDo toDo)
+        {
+
+            var getToDo = GetToDo(toDo.Id);
+           
+
+            if(getToDo == null)
+            {
+                return false;
+            }
+
+            getToDo.Description = toDo.Description;
+            getToDo.Priority = toDo.Priority;
+            getToDo.IsComplete = toDo.IsComplete;
+            _context.SaveChanges();
+
+            return true;
+        }
+
+        public bool DeleteToDo(int id)
+        {
+            var toDoRecord = _context.ToDos.Where(td => td.Id == id).FirstOrDefault();
+
+            try
+            {
+                _context.Remove(toDoRecord);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+}
